de7277a7dddf25facf7e0e916473749f6745d7ef
Thiseas Matthaioudakis <t.matthaioudakis@kaizengaming.com>
2024-11-24 20:42:31 +0200

    Add Context class

---
+++ Subscribo.Data/Context/SubscriboContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Subscribo.Data.DTOs;
+
+namespace Subscribo.Data.Context
+{
+    public class SubscriboContext : DbContext
+    {
+        public DbSet<CustomerDTO> Customers { get; set; }
+        public DbSet<InvoiceDTO> Invoices { get; set; }
+        public DbSet<SubscriptionDTO> Subscriptions { get; set; }
+
+        private string connectionString = string.Empty;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+         => optionsBuilder.UseNpgsql(connectionString);
+    }
+}
