1eea38ef66510a8d38652c51b376de385cd843bb
Thiseas Matthaioudakis <t.matthaioudakis@kaizengaming.com>
2024-11-26 01:54:47 +0200

    Add Customer Repository

---
+++ Subscribo.Data/Interfaces/ICustomerRepository.cs
@@ -0,0 +1,10 @@
+﻿using Subscribo.Data.DTOs;
+
+namespace Subscribo.Data.Interfaces
+{
+    public interface ICustomerRepository
+    {
+        Task CreateCustomerAsync(CustomerDTO customer, CancellationToken cancellationToken = default);
+        Task<CustomerDTO?> GetByIdAsync(int customerId, CancellationToken cancellationToken = default);
+    }
+}
---
+++ Subscribo.Data/Repositories/CustomerRepository.cs
@@ -0,0 +1,14 @@
+﻿using Subscribo.Data.Context;
+using Subscribo.Data.DTOs;
+using Subscribo.Data.Interfaces;
+
+namespace Subscribo.Data.Repositories
+{ 
+    public class CustomerRepository(SubscriboContext dbContext) : ICustomerRepository
+    {
+        public async Task CreateCustomerAsync(CustomerDTO customer, CancellationToken cancellationToken) => await dbContext.Customers.AddAsync(customer, cancellationToken);
+
+        public async Task<CustomerDTO?> GetByIdAsync(int customerId, CancellationToken cancellationToken) => await dbContext.Customers.FindAsync([customerId], cancellationToken); 
+        
+    }
+}
